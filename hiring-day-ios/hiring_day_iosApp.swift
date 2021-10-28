63e5604630b8cca4887478045806fc69b0f8f146
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-10-28 17:55:23 -0300

    basic structure

---
+++ hiring-day-ios/hiring_day_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiring_day_iosApp.swift
+//  hiring-day-ios
+//
+//  Created by bruno on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct hiring_day_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
