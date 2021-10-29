e268f5ca2d4fab6a8ce9549c8fff3024d0f4da05
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-10-28 23:34:32 -0300

    created a template of JSONHelper

---
+++ hiring-day-ios/Utils/JSONHelper.swift
@@ -0,0 +1,16 @@
+//
+//  JSONHelper.swift
+//  hiring-day-ios
+//
+//  Created by bruno on 28/10/21.
+//
+
+import Foundation
+
+final class JSONHelper {
+    
+    static func getArrayObjectFrom<T: Codable>(json file: String, type: T.Type) -> [T]? {
+        //here you should get a file Path() and convert into a Data(), then cast into an array of a decoded object [T].self
+        return nil
+    }
+}
